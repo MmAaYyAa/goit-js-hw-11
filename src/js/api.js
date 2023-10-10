2a029d06ce66cb5029576d388246ae81fb231eba
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-10-10 12:05:18 +0100

    styles

--- src/js/api.js
+++ src/js/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = 'https://pixabay.com/api/';
 
 const serviceGetPhotos = async (value, page) => {
-    const {data} = await axios.get('BASE_URL', {
+    const {data} = await axios.get(BASE_URL, {
         params: {
             key: "39898226-b5dd88d4a11b5ca39177fb963",
             q: `${value}`,
